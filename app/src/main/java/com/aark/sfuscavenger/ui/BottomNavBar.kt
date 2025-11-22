61745479a42441157078d668f66d8cf9866468ce
acey <adevjiani@gmail.com>
2025-11-21 16:52:36 -0800

    changed navbar icons

--- app/src/main/java/com/aark/sfuscavenger/ui/BottomNavBar.kt
+++ app/src/main/java/com/aark/sfuscavenger/ui/BottomNavBar.kt
@@ -2,6 +2,7 @@ package com.aark.sfuscavenger.ui
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
@@ -12,23 +13,36 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.aark.sfuscavenger.R
+import com.aark.sfuscavenger.ui.theme.White
 
 data class BottomNavItem(
     val label: String,
     val route: String,
-    val icon: androidx.compose.ui.graphics.vector.ImageVector
+    val icon: @Composable (selected: Boolean) -> Unit
 )
 
 @Composable
 fun BottomNavBar(navController: NavHostController) {
     val items = listOf(
-        BottomNavItem("Home", "home", Icons.Filled.Home),
-        BottomNavItem("Events", "events", Icons.Filled.Add),
+        BottomNavItem("Home", "home") { selected ->
+            Icon(imageVector = Icons.Filled.Home, contentDescription = "Home", tint = if(selected) Color.Unspecified else White)
+        },
+        BottomNavItem("Events", "events") { selected ->
+            Icon(imageVector = Icons.Filled.DateRange, contentDescription = "Events", tint = if(selected) Color.Unspecified else White)
+        },
         // Replace lock icon later
-        BottomNavItem("History", "history", Icons.Filled.Lock),
-        BottomNavItem("Social", "social", Icons.Filled.Person)
+        BottomNavItem("History", "history") { selected ->
+            Icon(
+                painter = painterResource(R.drawable.sc_history_48dp), contentDescription = "History", tint = if(selected) Color.Unspecified else White
+            )
+        },
+        BottomNavItem("Social", "social") { selected ->
+            Icon(imageVector = Icons.Filled.Person, contentDescription = "Social", tint = if(selected) Color.Unspecified else White)
+        }
     )
 
     NavigationBar(containerColor = Color(0xFFA6192E)) {
@@ -36,23 +50,23 @@ fun BottomNavBar(navController: NavHostController) {
         val currentRoute = navBackStackEntry?.destination?.route
 
         items.forEach { item ->
-
+            val selected = currentRoute == item.route
             NavigationBarItem(
-                selected = currentRoute == item.route,
+                selected = selected,
                 onClick = {
-
-                    if (currentRoute != item.route) {
+                    if (!selected) {
                         navController.navigate(item.route)
                     }
                 },
                 icon = {
-                    Icon(
-                        imageVector = item.icon,
-                        contentDescription = item.label,
-                        tint = Color.White
-                    )
+//                    Icon(
+//                        imageVector = item.icon,
+//                        contentDescription = item.label,
+//                        tint = Color.White
+//                    )
+                    item.icon(selected)
                 },
-                label = { Text(text = item.label, color = Color.White) }
+                label = { Text(text = item.label, color = White) }
             )
         }
     }
