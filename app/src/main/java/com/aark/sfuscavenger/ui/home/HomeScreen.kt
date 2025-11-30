9c1618222e6e9a7b5e61f097b673c4bce9f4ce0f
Karen Yao <karenyaooo@gmail.com>
2025-11-30 13:55:55 -0800

    circular indicator and card changes in theme

--- app/src/main/java/com/aark/sfuscavenger/ui/home/HomeScreen.kt
+++ app/src/main/java/com/aark/sfuscavenger/ui/home/HomeScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -88,7 +89,7 @@ fun HomeScreen(
                 ) {
                     ScavengerText(
                         text = activeGame.name,
-                        style = androidx.compose.material3.MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.SemiBold),
+                        style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.SemiBold),
                         color = Color.Black
                     )
                 }
